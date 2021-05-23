2b18a707848693f4ba23845ae0958471f5e9f453
anthony0824 <anthony58022@gmail.com>
2021-05-23 21:43:21 +0800

    Last Commit

--- ionic-firebase-setup/src/environments/environment.ts
+++ ionic-firebase-setup/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-    firebaseConfig: {
-    apiKey: "AIzaSyBGX0nhdoEM6mk8TjydqcmEUGEveIu961c",
-    authDomain: "activity5-1ac33.firebaseapp.com",
-    projectId: "activity5-1ac33",
-    storageBucket: "activity5-1ac33.appspot.com",
-    messagingSenderId: "268345154520",
-    appId: "1:268345154520:web:48879c0beee436899a30ad"
-  
-  }
+  production: false
 };
 
 /*
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig: {
+    apiKey: "AIzaSyBGX0nhdoEM6mk8TjydqcmEUGEveIu961c",
+    authDomain: "activity5-1ac33.firebaseapp.com",
+    projectId: "activity5-1ac33",
+    storageBucket: "activity5-1ac33.appspot.com",
+    messagingSenderId: "268345154520",
+    appId: "1:268345154520:web:48879c0beee436899a30ad"
+  }
+  
 };
